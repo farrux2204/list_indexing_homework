78e9462d7a1afee0c289d3be583708eea6da6e0c
Farrux <ulugbekovfarrux0822@gmail.com>
2022-10-21 09:16:37 +0500

    06

--- List06.py
+++ List06.py
@@ -6,4 +6,15 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    if list1[0]==1:
+        list1==True
+    if list1[1]==2:
+        list1==True
+    if list1[2]==3:
+        list1==True
+    if list1[3]==4:
+        list1==True
+    if list1[4]==5:
+        list1==True
+    return list1
+print(main([2,0,0,8,9]))
